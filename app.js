01f68960a50490ee4b8b4197a92064b933392dbc
soojene <73275204+soojene@users.noreply.github.com>
2021-01-12 18:05:20 +0900

    js beginPath on canvas
    with only one color and one size of linewidth

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
 
 ctx.strockStyle = '#000000';
-ctx.lineWidth = 2.5; 
+ctx.lineWidth = 2.5;
 
 let painting = false;
+
+//클래스이름으로 캔버스 사이즈 값을 설정할때. 이렇게 하거나 아래 함수로 해도 화면 사이즈가 바뀔때 새로고침을 해야 함..
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
+
+// function canvasContainner (canvas){ //캔버스 사이즈를 함수로 나타 낼 경우
+//     canvas.style.width = '90%';
+//     canvas.style.height = '70vh';
+//     canvas.width = canvas.offsetWidth;
+//     canvas.height = canvas.offsetHeight;
+// }
+// canvasContainner(canvas);
 
 function stopPainting (){
     painting = false; 
@@ -19,13 +31,18 @@ function startPainting (){
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
-    // console.log("mouse move");
+    if (!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
-function onMouseDown (event){
-    painting = true;
-    // console.log("mouse down");
-}
+// function onMouseDown (event){
+//     painting = true;
+// }
 
 if (canvas){
     canvas.addEventListener("mousemove", onMouseMove);
